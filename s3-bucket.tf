a484c69dac8b7f3766f1d1e3088860a5d3264170
Fabricio Obregon <fabricioobregon@live.com>
2022-05-03 18:14:21 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/HCTFPO/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket-bwcpiqefbucqwpeqwe"
   acl    = "private"
 
   versioning = {
